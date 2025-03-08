c5dcbfa9776916ae0fa5b32e1413d857c60f9370
Jotave <joaovitor.ss2004@gmail.com>
2025-03-08 10:14:19 -0300

    aulas herança

---
+++ primeira-aula-java/src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Endereco.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.dominio;
+
+public class Endereço {
+    private String rua;
+    private String cep;
+
+    public Endereço(String rua, String cep){
+        this.rua = rua;
+        this.cep = cep;
+    }
+    public void setRua(String rua){
+        this.rua = rua;
+    }
+    public String getRua(){
+        return rua;
+    }
+    public void setCep(String cep){
+        this.cep = cep;
+    }
+    public String getCep(){
+        return cep;
+    }
+
+}
